0f94b62dadf65b0d52b46e68bd53a8e00794171a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-23 17:29:03 +0000

    Deploy denisstasyev/2018-FS-11-Frontend-Stasyev to github.com/denisstasyev/2018-FS-11-Frontend-Stasyev.git:gh-pages

---
+++ precache-manifest.ff1f2f20fc7188bc0f325bb8781ae286.js
@@ -0,0 +1,45 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcd2be850f06399216e1",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/css/main.dee78154.chunk.css"
+  },
+  {
+    "revision": "bcd2be850f06399216e1",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/js/main.bcd2be85.chunk.js"
+  },
+  {
+    "revision": "9c0d8a237651602a6601",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/js/runtime~main.9c0d8a23.js"
+  },
+  {
+    "revision": "dc62e8f164cc67b732c6",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/js/2.dc62e8f1.chunk.js"
+  },
+  {
+    "url": "/2018-FS-11-Frontend-Stasyev/reactions/sprite.png?d6f4a939bc19664745e5bb6f7626b6fa"
+  },
+  {
+    "revision": "aa84ce7c508f1f01090f3476a9581fd1",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/media/home-icon.aa84ce7c.svg"
+  },
+  {
+    "revision": "fe203679c16ddcc6a27a2fb5f1873c15",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/media/paper-plane.fe203679.svg"
+  },
+  {
+    "revision": "496565f12349ab4e4a4dfeb73d8c7b5e",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/media/paperclip.496565f1.svg"
+  },
+  {
+    "revision": "c6fe84c804f1b13624b4ffda82ef5ffd",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/media/map-marker.c6fe84c8.svg"
+  },
+  {
+    "revision": "9e7acdb46f2bee8c4d0cc59e838777b5",
+    "url": "/2018-FS-11-Frontend-Stasyev/static/media/sign-add.9e7acdb4.svg"
+  },
+  {
+    "revision": "62119999fd22b8f8fb285de6cf691332",
+    "url": "/2018-FS-11-Frontend-Stasyev/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/2018-FS-11-Frontend-Stasyev/precache-manifest.ff1f2f20fc7188bc0f325bb8781ae286.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/2018-FS-11-Frontend-Stasyev/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
